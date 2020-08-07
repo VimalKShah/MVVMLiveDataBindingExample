974131753bb437c4236777a6abc6e19504290af2
VimalKShah <vshah925@gmail.com>
2020-08-07 20:13:49 +0530

    MVVM LiveData Binding Example.
    - Use ViewModel to update the view instead of observable.

--- MVVMLiveDataBindingExample/app/src/main/java/com/example/mvvmlivedatabindingexample/LoginViewModel.java
+++ MVVMLiveDataBindingExample/app/src/main/java/com/example/mvvmlivedatabindingexample/LoginViewModel.java
@@ -2,81 +2,54 @@ package com.example.mvvmlivedatabindingexample;
 
 import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.databinding.BaseObservable;
-import androidx.databinding.Bindable;
-import androidx.databinding.ObservableField;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
-public class LoginViewModel extends BaseObservable {
-    private String email;
-    private String password;
-    private int busy = 8;
+public class LoginViewModel extends ViewModel {
+    public MutableLiveData<String> email = new MutableLiveData<>();
+    public MutableLiveData<String> password = new MutableLiveData<>();
+    public MutableLiveData<Integer> busy;
 
-    public final ObservableField<String> errorEmail = new ObservableField<>();
-    public final ObservableField<String> errorPassword = new ObservableField<>();
+    public final MutableLiveData<String> errorEmail = new MutableLiveData<>();
+    public final MutableLiveData<String> errorPassword = new MutableLiveData<>();
 
     private MutableLiveData<User> userMutableLiveData;
 
     public LoginViewModel() {
     }
 
-    public LiveData<User> getUser() {
+    public MutableLiveData<User> getUser() {
         if(userMutableLiveData == null) {
             userMutableLiveData = new MutableLiveData<>();
         }
         return userMutableLiveData;
     }
 
-    @Bindable
-    @NonNull
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-        notifyPropertyChanged(BR.email);
-    }
-
-    @Bindable
-    @NonNull
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-        notifyPropertyChanged(BR.password);
-    }
-
-    @Bindable
-    public int getBusy() {
+    public LiveData<Integer> getBusy() {
+        if(busy == null) {
+            busy = new MutableLiveData<>();
+            busy.setValue(8);
+        }
         return busy;
     }
 
-    public void setBusy(int busy) {
-        this.busy = busy;
-        notifyPropertyChanged(BR.busy);
-    }
-
     public void OnLoginClicked(View view) {
-        setBusy(0);
-        User user = new User(getEmail(), getPassword());
+        busy.setValue(0);
+        User user = new User(email.getValue(), password.getValue());
         if (!user.isEmailValid()) {
-            errorEmail.set("Enter a valid email address");
+            errorEmail.setValue("Enter a valid email address");
         } else {
-            errorEmail.set(null);
+            errorEmail.setValue(null);
         }
 
         if (!user.isPasswordLengthGreaterThan5())
-            errorPassword.set("Password Length should be greater than 5");
+            errorPassword.setValue("Password Length should be greater than 5");
         else {
-            errorPassword.set(null);
+            errorPassword.setValue(null);
         }
         userMutableLiveData.setValue(user);
-        setBusy(8);
+        busy.setValue(8);
     }
 
 }
--- MVVMLiveDataBindingExample/app/src/main/java/com/example/mvvmlivedatabindingexample/MainActivity.java
+++ MVVMLiveDataBindingExample/app/src/main/java/com/example/mvvmlivedatabindingexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mvvmlivedatabindingexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        LoginViewModel loginViewModel = new LoginViewModel();
+        LoginViewModel loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
         mainBinding.setLoginViewModel(loginViewModel);
+        mainBinding.setLifecycleOwner(this);
 
         loginViewModel.getUser().observe(this, new Observer<User>() {
             @Override
